55f422ba77a629bbf619eb7184378540ca8616b8
Ivan-Benegas <38094339+Ivan-Benegas@users.noreply.github.com>
2021-10-28 20:49:43 -0300

    Add files via upload

---
+++ Mesa5/src/main/java/com/Mesa5/Mesa5/entity/Gitgorra.java
@@ -0,0 +1,13 @@
+package com.Mesa5.Mesa5.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Gitgorra {
+
+	@Id
+	private String id;
+	private Boolean odioPorGit = true;
+	private Boolean conocimiento = true;
+}
---
+++ Mesa5/src/main/java/com/Mesa5/Mesa5/entity/Usuario.java
@@ -0,0 +1,12 @@
+package com.Mesa5.Mesa5.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	private String id;
+	
+}
