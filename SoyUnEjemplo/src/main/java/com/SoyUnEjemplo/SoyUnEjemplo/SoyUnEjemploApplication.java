178b5edb5c6a8dae566b36ee5c604033cc2c2c4a
Franco Lamberti <francolamberti02@gmail.com>
2021-10-28 19:01:58 -0300

    Me cago en Git

---
+++ SoyUnEjemplo/src/main/java/com/SoyUnEjemplo/SoyUnEjemplo/SoyUnEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.SoyUnEjemplo.SoyUnEjemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoyUnEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoyUnEjemploApplication.class, args);
+	}
+
+}
